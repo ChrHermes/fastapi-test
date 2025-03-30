e0d3fa6459d93f1b130470ef7bd48b3e4395d38a
Christopher Hermes <chrhermes@gmx.de>
2025-03-30 22:17:49 +0200

    Design-Anpassungen

--- static/script/script.js
+++ static/script/script.js
@@ -1,45 +1,46 @@
 /* ---------------------------------------------- EVENT LISTENER */
-document.addEventListener("DOMContentLoaded", function () {
-    const themeToggle = document.getElementById("theme-toggle");
+document.addEventListener('DOMContentLoaded', function () {
+    const themeToggle = document.getElementById('theme-toggle')
+    const themeIcon = document.getElementById('theme-icon')
 
-    if (localStorage.getItem("theme") === "dark") {
-        document.body.classList.add("dark-mode");
-        themeToggle.textContent = "🌞";
+    if (localStorage.getItem('theme') === 'dark') {
+        document.body.classList.add('dark-mode')
+        themeIcon.textContent = 'light_mode'
     } else {
-        themeToggle.textContent = "🌚";
+        themeIcon.textContent = 'dark_mode'
     }
 
-    themeToggle.addEventListener("click", function () {
-        document.body.classList.toggle("dark-mode");
-        const isDark = document.body.classList.contains("dark-mode");
-        localStorage.setItem("theme", isDark ? "dark" : "light");
-        themeToggle.textContent = isDark ? "🌞" : "🌚";
-    });
-});
+    themeToggle.addEventListener('click', function () {
+        document.body.classList.toggle('dark-mode')
+        const isDark = document.body.classList.contains('dark-mode')
+        localStorage.setItem('theme', isDark ? 'dark' : 'light')
+        themeIcon.textContent = isDark ? 'light_mode' : 'dark_mode'
+    })
+})
 
 /* ---------------------------------------------- LOGIN */
 async function sendRequest(endpoint) {
     try {
         const response = await fetch(endpoint, {
-            method: "POST",
-            headers: { 'Authorization': 'Basic ' + btoa('admin:password') }
-        });
-        const data = await response.json();
-        appendLog(data.message);
+            method: 'POST',
+            headers: { Authorization: 'Basic ' + btoa('admin:password') },
+        })
+        const data = await response.json()
+        appendLog(data.message)
     } catch (error) {
-        console.error("Fehler beim Senden der Anfrage:", error);
+        console.error('Fehler beim Senden der Anfrage:', error)
     }
 }
 
 // Automatische Umleitung zur Login-Seite, falls nicht eingeloggt
-(async function checkLogin() {
+;(async function checkLogin() {
     try {
-        const response = await fetch("/protected");
+        const response = await fetch('/protected')
         if (!response.ok) {
-            window.location.href = "/login";
+            window.location.href = '/login'
         }
     } catch (error) {
-        console.error("Fehler bei der Login-Prüfung:", error);
-        window.location.href = "/login";
+        console.error('Fehler bei der Login-Prüfung:', error)
+        window.location.href = '/login'
     }
-})();
+})()
